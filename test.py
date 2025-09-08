078ac77071a312bcd2352ccf0829767e20e88a93
Aron Alander <aar20007@student.mdu.se>
2025-09-08 13:17:43 +0200

    FIX

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print("Hej gruppen!")
 for i in range(5):
     print('*' * (i + 1))
+print("CUP:99%")
 # actually Aron was here before  🤓 2025-09-05
 #Jonathan was here! 2025-09-05
 #David Was here 2025-09-05
