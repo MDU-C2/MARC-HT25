d2e5e0f1495729fdc5edc419a96c1b4705acdafd
David-Bogren <dbn19002@student.mdu.se>
2025-11-06 11:01:05 +0100

    updated test protocol file to add more information

--- test_protocol.py
+++ test_protocol.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 def create_today_textfile():
-    today_str = datetime.now().strftime("%Y-%m-%d")
+    today_str = datetime.now().strftime("%Y-%m-%d_%H.%M")
     folder = "Test_Protocols"
     os.makedirs(folder, exist_ok=True)
     filename = os.path.join(folder, f"{today_str}.txt")
@@ -10,3 +10,27 @@ def create_today_textfile():
         with open(filename, "w") as f:
             f.write(f"File created on {today_str}\n")
     return filename
+
+
+def fill_meta_data(file_path):
+    with open(file_path, "a") as f:
+        f.write("---------- Meta Data ----------\n")
+        try:
+            with open("saved_coordinates.txt", "r") as coord_file:
+                num_lines = sum(1 for _ in coord_file)
+        except FileNotFoundError:
+            num_lines = 0
+        f.write(f"Number of calibration positions: {num_lines}\n")
+
+
+def ask_user(file_path):
+    response = input("What variable was tested? ")
+    with open(file_path, "a") as f:
+        f.write("---------- Test Information ----------\n")
+        f.write(f"Tested Variable: {response}\n")
+
+
+# main
+file_path = create_today_textfile()
+fill_meta_data(file_path)
+ask_user(file_path)
